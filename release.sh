c2577558099bb8fabb9e62ec264aa2a6d7045a43
Sebastian Menski <sebastian.menski@camunda.com>
2018-05-24 13:49:57 +0200

    ci(travis): do not push to docker hub for development branches

--- release.sh
+++ release.sh
@@ -15,6 +15,11 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     exit 0
 fi
 
+if ! [[ "${TRAVIS_BRANCH}" =~ master|7.[0-9]+|next ]]; then
+    echo "Not pushing development branch to docker hub"
+    exit 0
+fi
+
 if [ "${EE}" = "true" ]; then
     echo "Not pushing EE image to docker hub"
     exit 0
