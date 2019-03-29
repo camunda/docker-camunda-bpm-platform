05d1f7b06f6da2e4aa3167b11b4303963fd889a4
Philipp Heuer <git@philippheuer.me>
2019-03-29 22:36:42 +0100

    feat(db): set flyway migration path based on the used jdbc driver

--- global/extension.d/flyway/flyway.pre-start.sh
+++ global/extension.d/flyway/flyway.pre-start.sh
@@ -5,7 +5,29 @@ set -o errexit
 echo "[FlyWay Migration: $DB_MIGRATION]"
 
 # Run Db Migration
-if [ "$DB_MIGRATION" = true ] ; then
+if [ "$DB_MIGRATION" = true ]; then
+    # determinate the correct db type for flyway
+    DB_TYPE=h2
+
+    if [ "$DB_DRIVER" = "COM.ibm.db2.jdbc.app.DB2Driver" ]; then
+        DB_TYPE=db2
+    fi
+    if [ "$DB_DRIVER" = "com.mysql.cj.jdbc.Driver" ]; then
+        DB_TYPE=mysql
+    fi
+    if [ "$DB_DRIVER" = "com.mariadb.jdbc.Driver" ]; then
+        DB_TYPE=mariadb
+    fi
+    if [ "$DB_DRIVER" = "com.microsoft.sqlserver.jdbc.SQLServerDriver" ]; then
+        DB_TYPE=mssql
+    fi
+    if [ "$DB_DRIVER" = "org.postgresql.Driver" ]; then
+        DB_TYPE=postgres
+    fi
+    if [ "$DB_DRIVER" = "oracle.jdbc.driver.OracleDriver" ]; then
+        DB_TYPE=oracle
+    fi
+
     echo "  Running db migrations for camunda $CAMUNDA_VERSION ..."
     flyway \
         -user=$DB_USERNAME \
@@ -16,7 +38,7 @@ if [ "$DB_MIGRATION" = true ] ; then
         -baselineVersion=$CAMUNDA_VERSION \
         -baselineOnMigrate=true \
         -baselineDescription=$CAMUNDA_VERSION \
-        -locations=filesystem:/sql/h2 \
+        -locations=filesystem:/sql/$DB_TYPE \
         -cleanDisabled=true \
         migrate
 fi
