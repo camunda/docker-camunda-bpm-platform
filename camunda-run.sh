44384e252ef25ad22ea2e50198986aed03fe39aa
Leonhardt Wille <leonhardt.wille@camunda.com>
2020-03-30 19:32:51 +0200

    chore: add DB_PASSWORD_FILE support for camunda-run
    related to INFRA-1124

--- camunda-run.sh
+++ camunda-run.sh
@@ -9,6 +9,16 @@ if [[ -z "${DB_PASSWORD}" ]]; then
   export DB_PASSWORD="sa"
 fi
 
+# Set Password as Docker Secrets for Swarm-Mode
+if [[ -z "${DB_PASSWORD:-}" && -n "${DB_PASSWORD_FILE:-}" && -f "${DB_PASSWORD_FILE:-}" ]]; then
+  password="$(< "${DB_PASSWORD_FILE}")"
+  export DB_PASSWORD="$password"
+fi
+
+if [[ -z "${DB_PASSWORD}" ]]; then
+  export DB_PASSWORD="sa"
+fi
+
 export SPRING_DATASOURCE_DRIVER_CLASS_NAME=${DB_DRIVER:-org.h2.Driver}
 export SPRING_DATASOURCE_PASSWORD=${DB_PASSWORD:-}
 export SPRING_DATASOURCE_USERNAME=${DB_USERNAME:-}
