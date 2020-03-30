68461d81d65516e1e868682ac446767e9026d3d2
Leonhardt Wille <leonhardt.wille@camunda.com>
2020-03-30 16:40:07 +0200

    chore(test): add test for camunda-run image
    related to INFRA-1124

---
+++ test/test-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+SERVICE=${1}
+
+source test_helper.sh
+
+start_container
+
+poll_log "starting to acquire jobs" "Application run failed" || _exit 1 "Server not started"
+
+_log "Server started"
+
+test_login admin || _exit 3 "Unable to login to admin"
+test_login cockpit || _exit 4 "Unable to login to cockpit"
+test_login tasklist || _exit 5 "Unable to login to tasklist"
+
+_log "Login successfull"
+
+test_encoding || _exit 6 "Wrong encoding detected"
+
+_exit 0 "Test successfull"
