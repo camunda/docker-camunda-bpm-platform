db0c35bccb4b3ac00e417864eb72bd5d53684e62
Max Danilov <maxim.danilov@camunda.com>
2020-07-20 17:23:39 +0200

    chore(ci): Do not run EE builds for fork PRs
    Nexus EE credentials are not available for forks
    
    Related: INFRA-725

---
+++ pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+docker build .                                \
+    -t camunda/camunda-bpm-platform:${DISTRO} \
+    --build-arg DISTRO=${DISTRO}              \
+    --build-arg EE=${EE}                      \
+    --build-arg USER=${NEXUS_USER}            \
+    --build-arg PASSWORD=${NEXUS_PASS}
+
+./test/test.sh
+./release.sh
