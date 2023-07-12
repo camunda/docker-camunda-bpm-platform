6b7c6cb7f117d1e68dcfb8189f456efa4eab4069
Yana Vasileva <yana.vasileva@camunda.com>
2023-07-12 17:44:23 +0200

    fix(ci): trigger maintenance snapshot version
    related to camunda/camunda-bpm-platform#3515

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,24 @@
-#!/bin/bash
- 
+#!/bin/bash -ex
+
+if [ -z "$SNAPSHOT" ]; then
+  SNAPSHOT_ARGUMENT=""
+else
+  SNAPSHOT_ARGUMENT="--build-arg SNAPSHOT=${SNAPSHOT}"
+fi
+
+if [ -z "$VERSION" ]; then
+  VERSION_ARGUMENT=""
+else
+  VERSION_ARGUMENT="--build-arg VERSION=${VERSION}"
+fi
+
 docker build .                                \
     -t camunda/camunda-bpm-platform:${DISTRO} \
     --build-arg DISTRO=${DISTRO}              \
     --build-arg EE=${EE}                      \
     --build-arg USER=${NEXUS_USER}            \
+    ${VERSION_ARGUMENT}                       \
+    ${SNAPSHOT_ARGUMENT}                      \
     --build-arg PASSWORD=${NEXUS_PASS}
 
 ./test/test.sh
