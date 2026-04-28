0afcdd2560140773541151fb158bf726478513be
Igor Petrov <igor.petrov-ext@camunda.com>
2026-04-28 11:45:47 +0300

    revert: feat: migrate to native runners (#333)
    This reverts commit 183990ad9cdfe0eef2eb2c5b4dbf302733a7b88d.

--- test/test.sh
+++ test/test.sh
@@ -4,21 +4,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd ${DIR}
 
-# Ensure compose teardown runs even on failures, preserving the original exit code
-trap 'rc=$?; docker compose down -v || true; exit $rc' EXIT
-
-function run_test {
-  echo "::group::Test '$2'"
-  local rc=0
-  "$@" || rc=$?
-  echo "::endgroup::"
-  return $rc
-}
+source test_helper.sh
 
 docker compose up --force-recreate -d postgres mysql
-run_test ./test-${DISTRO}.sh camunda
-run_test ./test-${DISTRO}.sh camunda-mysql
-run_test ./test-${DISTRO}.sh camunda-postgres
-run_test ./test-${DISTRO}.sh camunda-password-file
-run_test ./test-prometheus-jmx-${DISTRO}.sh camunda-prometheus-jmx
-run_test ./test-debug.sh camunda-debug
+./test-${DISTRO}.sh camunda
+./test-${DISTRO}.sh camunda-mysql
+./test-${DISTRO}.sh camunda-postgres
+./test-${DISTRO}.sh camunda-password-file
+./test-prometheus-jmx-${DISTRO}.sh camunda-prometheus-jmx
+./test-debug.sh camunda-debug
+docker compose down -v
+cd -
