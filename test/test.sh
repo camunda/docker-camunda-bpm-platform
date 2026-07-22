aadfc65eded98c62786e47ab629cbd678ecf3ace
oleksiimitrovcmnd <oleksii.mitrov-ext@camunda.com>
2026-07-22 11:45:43 +0200

    feat(build): migrate from QEMU to native ARM runners - migrate from QEMU to native ARM runners Related to: https://github.com/camunda/camunda-bpm-platform-maintenance/issues/3006

--- test/test.sh
+++ test/test.sh
@@ -4,14 +4,21 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd ${DIR}
 
-source test_helper.sh
+# Ensure compose teardown runs even on failures, preserving the original exit code
+trap 'rc=$?; docker compose down -v || true; exit $rc' EXIT
+
+function run_test {
+  echo "::group::Test '$2'"
+  local rc=0
+  "$@" || rc=$?
+  echo "::endgroup::"
+  return $rc
+}
 
 docker compose up --force-recreate -d postgres mysql
-./test-${DISTRO}.sh camunda
-./test-${DISTRO}.sh camunda-mysql
-./test-${DISTRO}.sh camunda-postgres
-./test-${DISTRO}.sh camunda-password-file
-./test-prometheus-jmx-${DISTRO}.sh camunda-prometheus-jmx
-./test-debug.sh camunda-debug
-docker compose down -v
-cd -
+run_test ./test-${DISTRO}.sh camunda
+run_test ./test-${DISTRO}.sh camunda-mysql
+run_test ./test-${DISTRO}.sh camunda-postgres
+run_test ./test-${DISTRO}.sh camunda-password-file
+run_test ./test-prometheus-jmx-${DISTRO}.sh camunda-prometheus-jmx
+run_test ./test-debug.sh camunda-debug
